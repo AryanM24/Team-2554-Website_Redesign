d216af6a00f60b1b50225a78a736c69eb3603f65
Abir Modak <abrmod74@gmail.com>
2025-03-23 13:10:02 -0400

    moved json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("cc9b2b7408ba2292")
+/******/ 		__webpack_require__.h = () => ("41f2f89646b7352a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3b86b4afc655f059"; }
+/******/ 		__webpack_require__.h = function() { return "555dfbb3d675fbca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
